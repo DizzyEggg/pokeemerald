5d00478c2d5eda244863d5091e5cbb4920d874c9
Eduardo Quezada D'Ottone <eduardo602002@gmail.com>
2019-09-15 22:34:58 -0300

    Gen7 trainer exp multiplier.

--- include/constants/battle_config.h
+++ include/constants/battle_config.h
@@ -50,6 +50,7 @@
 #define B_SOUND_SUBSTITUTE          GEN_6 // Starting from gen6 sound moves bypass Substitute.
 #define B_EXP_CATCH                 GEN_6 // Starting from gen6, pokemon get experience from catching.
 #define B_ABILITY_POP_UP            GEN_6 // Starting from gen5, the pokemon abilities are displayed in a pop-up, when they activate in battle.
+#define B_TRAINER_BATTLE_MULTIPLIER GEN_6 // Gen7 no longer gives a 1.5 multiplier to exp gain.
 
 #define B_FAST_INTRO                TRUE // If set to TRUE, battle intro texts print at the same time as animation of a pokemon, as opposing to waiting for the animation to end.
 
--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3435,7 +3435,7 @@ static void atk23_getexp(void)
                         gBattleMoveDamage += gExpShareExp;
                     if (holdEffect == HOLD_EFFECT_LUCKY_EGG)
                         gBattleMoveDamage = (gBattleMoveDamage * 150) / 100;
-                    if (gBattleTypeFlags & BATTLE_TYPE_TRAINER)
+                    if (gBattleTypeFlags & BATTLE_TYPE_TRAINER && B_TRAINER_BATTLE_MULTIPLIER <= GEN_6)
                         gBattleMoveDamage = (gBattleMoveDamage * 150) / 100;
 
                     if (IsTradedMon(&gPlayerParty[gBattleStruct->expGetterMonId]))
