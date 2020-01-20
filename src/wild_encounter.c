58df025b612288180192b20ad4681d789a534e52
Eduardo Quezada <eduardo602002@gmail.com>
2020-01-20 11:32:48 -0300

    Quick Feet's overworld effect. (reduces the chance of encountering wild Pokémon by 50%)

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -500,6 +500,8 @@ static bool8 DoWildEncounterRateTest(u32 encounterRate, bool8 ignoreAbility)
             encounterRate /= 2;
         else if (ability == ABILITY_SNOW_CLOAK && gSaveBlock1Ptr->weather == 4)
             encounterRate /= 2;
+        else if (ability == ABILITY_QUICK_FEET)
+            encounterRate /= 2;
     }
     if (encounterRate > 2880)
         encounterRate = 2880;
