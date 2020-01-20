b1e3734a23980f1120c0050327d62acf056b5ce1
Eduardo Quezada <eduardo602002@gmail.com>
2020-01-20 11:30:51 -0300

    Snow Cloak's overworld effect. (reduces the chance of encountering wild Pokémon on Snow by 50%)

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -498,6 +498,8 @@ static bool8 DoWildEncounterRateTest(u32 encounterRate, bool8 ignoreAbility)
             encounterRate *= 2;
         else if (ability == ABILITY_SAND_VEIL && gSaveBlock1Ptr->weather == 8)
             encounterRate /= 2;
+        else if (ability == ABILITY_SNOW_CLOAK && gSaveBlock1Ptr->weather == 4)
+            encounterRate /= 2;
     }
     if (encounterRate > 2880)
         encounterRate = 2880;
