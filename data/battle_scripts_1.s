6e8ba15e17f9a4291cc8812f0c487732a16ab4b9
Eduardo Quezada <eduardo602002@gmail.com>
2020-03-06 16:37:31 -0300

    Mind Blown Part 2 (Damp interaction)

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -2186,23 +2186,14 @@ BattleScript_EffectMindBlown::
 	attackcanceler
 	attackstring
 	ppreduce
-	@faintifabilitynotdamp
-	
-	@setatkhptozero
-
-	@waitanimation
+	faintifabilitynotdamp
 	dmg_1_2_attackerhp
-	@manipulatedamage DMG_CHANGE_SIGN
 	healthbarupdate BS_ATTACKER
 	datahpupdate BS_ATTACKER
-
 	waitstate
 	jumpifbyte CMP_NO_COMMON_BITS, gMoveResultFlags, MOVE_RESULT_MISSED, BattleScript_ExplosionDoAnimStartLoop
 	call BattleScript_PreserveMissedBitDoMoveAnim
 	goto BattleScript_ExplosionLoop
-
-	
-	
 
 BattleScript_PreserveMissedBitDoMoveAnim:
 	bichalfword gMoveResultFlags, MOVE_RESULT_MISSED
