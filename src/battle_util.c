5feb8279de4f0a8688f89903e1a4b1b0a0847ffc
Eduardo Quezada <eduardo602002@gmail.com>
2020-03-05 23:04:36 -0300

    Fixed Ability Pop Up for Immunity-type abilities with Mold Breaker
    When a mon with Mold Breaker caused a status move to another one with an Immunity-type ability such as that mon healed inmiediatly from it, the ability pop up would incorrectly show the attacker's ability instead.

--- src/battle_util.c
+++ src/battle_util.c
@@ -3620,6 +3620,7 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u8 ability, u8 special, u16 moveA
                 gBattlescriptCurrInstr = BattleScript_AbilityCuredStatus;
                 gBattleScripting.battler = battler;
                 gActiveBattler = battler;
+                gBattlerAbility = battler;
                 BtlController_EmitSetMonData(0, REQUEST_STATUS_BATTLE, 0, 4, &gBattleMons[gActiveBattler].status1);
                 MarkBattlerForControllerExec(gActiveBattler);
                 return effect;
