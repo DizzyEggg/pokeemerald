d3997fa18bb53652329669b6eda48fa6269628fa
AsparagusEduardo <eduardo602002@gmail.com>
2020-03-14 09:48:47 -0300

    Can poison itself with Toxic Orb.

--- src/battle_util.c
+++ src/battle_util.c
@@ -4639,7 +4639,9 @@ u8 ItemBattleEffects(u8 caseID, u8 battlerId, bool8 moveTurn)
         {
         case HOLD_EFFECT_TOXIC_ORB:
             if (!gBattleMons[battlerId].status1
-                && !IS_BATTLER_OF_TYPE(battlerId, TYPE_POISON) && !IS_BATTLER_OF_TYPE(battlerId, TYPE_STEEL)
+                && ((!IS_BATTLER_OF_TYPE(gEffectBattler, TYPE_POISON)
+                    && !IS_BATTLER_OF_TYPE(gEffectBattler, TYPE_STEEL))
+                    || GetBattlerAbility(battlerId) == ABILITY_CORROSION)
                 && GetBattlerAbility(battlerId) != ABILITY_IMMUNITY)
             {
                 effect = ITEM_STATUS_CHANGE;
