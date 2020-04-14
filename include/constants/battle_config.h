f10b8932a4714a07279787f99ac2624ff6e4e38c
MandL27 <mandl27g@gmail.com>
2020-04-14 15:48:11 -0400

    add more xp function options

--- include/constants/battle_config.h
+++ include/constants/battle_config.h
@@ -51,6 +51,8 @@
 #define B_CRIT_CHANCE               GEN_6 // Chances of a critical hit landing. See atk04_critcalc.
 #define B_CRIT_MULTIPLIER           GEN_6 // Starting from gen6, critical hits multiply damage by 1.5 instead of 2.
 #define B_EXP_CATCH                 GEN_6 // Starting from gen6, pokemon get experience from catching.
+#define B_SCALED_EXP                GEN_6 // In Gen5 and Gen7, exp scales by level of pokemon defeated.
+#define B_SPLIT_EXP                 GEN_5 // Starting from Gen6, exp per mon isn't reduced for using multiple mon.
 #define B_TRAINER_EXP_MULTIPLIER    GEN_6 // Gen7 no longer gives a 1.5 multiplier to exp gain in trainer battles.
 #define B_BURN_DAMAGE               GEN_6 // In Gen7, burn damage is 1/16th of max HP instead of 1/8th.
 #define B_PARALYSIS_SPEED           GEN_6 // In Gen7, speed is decreased by 50% instead of 75%.
@@ -61,10 +63,10 @@
 #define B_TOXIC_NEVER_MISS          GEN_6 // Starting from Gen6, if Toxic is used by a Poison type, it will never miss.
 
 // Ability settings
-#define B_ABILITY_POP_UP            GEN_6 // Starting from gen5, the pokemon abilities are displayed in a pop-up, when they activate in battle.
-#define B_ABILITY_WEATHER           GEN_6 // Up to gen5 - weather induced by abilities such as Drought or Drizzle lasted till the battle's end or weather change by a move. From Gen6 onwards, weather caused by abilities lasts the same amount of turns as induced from a move.
+#define B_ABILITY_POP_UP            GEN_6 // Starting from Gen5, the pokemon abilities are displayed in a pop-up, when they activate in battle.
+#define B_ABILITY_WEATHER           GEN_6 // Up to Gen5 - weather induced by abilities such as Drought or Drizzle lasted till the battle's end or weather change by a move. From Gen6 onwards, weather caused by abilities lasts the same amount of turns as induced from a move.
 #define B_GALE_WINGS                GEN_6 // Gen7 requires full hp.
-#define B_STANCE_CHANGE_FAIL        GEN_7 // In Gen7, Aegislash's form change does not happen, if the pokemon cannot use a move, because of confusion, paralysis, etc. In gen6, the form change occurs despite not being able to move.
+#define B_STANCE_CHANGE_FAIL        GEN_7 // In Gen7, Aegislash's form change does not happen, if the pokemon cannot use a move, because of confusion, paralysis, etc. In Gen6, the form change occurs despite not being able to move.
 #define B_GHOSTS_ESCAPE             GEN_6 // From Gen6 onwards, ghosts can escape even when blocked by abilities such as Shadow Tag.
 
 // Other
