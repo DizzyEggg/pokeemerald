61ab24ed0554b388c2502d2c7aa80089222aa13a
AsparagusEduardo <eduardo602002@gmail.com>
2020-04-15 08:25:28 -0400

    Using weather constants.

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -23,6 +23,7 @@
 #include "constants/layouts.h"
 #include "constants/maps.h"
 #include "constants/species.h"
+#include "constants/weather.h"
 
 extern const u8 EventScript_RepelWoreOff[];
 
@@ -496,9 +497,9 @@ static bool8 DoWildEncounterRateTest(u32 encounterRate, bool8 ignoreAbility)
             encounterRate /= 2;
         else if (ability == ABILITY_ARENA_TRAP)
             encounterRate *= 2;
-        else if (ability == ABILITY_SAND_VEIL && gSaveBlock1Ptr->weather == 8)
+        else if (ability == ABILITY_SAND_VEIL && gSaveBlock1Ptr->weather == WEATHER_SANDSTORM)
             encounterRate /= 2;
-        else if (ability == ABILITY_SNOW_CLOAK && gSaveBlock1Ptr->weather == 4)
+        else if (ability == ABILITY_SNOW_CLOAK && gSaveBlock1Ptr->weather == WEATHER_SNOW)
             encounterRate /= 2;
         else if (ability == ABILITY_QUICK_FEET)
             encounterRate /= 2;
