d8b8205b2f8a180641d74744449e87e4895654e6
LOuroboros <lunosouroboros@gmail.com>
2020-07-21 12:27:34 -0300

    Defined the sprites for Zygarde's 10% and Complete forms

--- include/graphics.h
+++ include/graphics.h
@@ -3717,6 +3717,14 @@ extern const u32 gMonFrontPic_LycanrocDusk[];
 extern const u32 gMonBackPic_LycanrocDusk[];
 extern const u32 gMonPalette_LycanrocDusk[];
 extern const u32 gMonShinyPalette_LycanrocDusk[];
+extern const u32 gMonFrontPic_Zygarde10[];
+extern const u32 gMonBackPic_Zygarde10[];
+extern const u32 gMonPalette_Zygarde10[];
+extern const u32 gMonShinyPalette_Zygarde10[];
+extern const u32 gMonFrontPic_Zygarde100[];
+extern const u32 gMonBackPic_Zygarde100[];
+extern const u32 gMonPalette_Zygarde100[];
+extern const u32 gMonShinyPalette_Zygarde100[];
 
 extern const u8 gMonIcon_QuestionMark[];
 extern const u8 gMonIcon_Bulbasaur[];
@@ -4652,6 +4660,8 @@ extern const u8 gMonIcon_KyuremWhite[];
 extern const u8 gMonIcon_MimikyuBusted[];
 extern const u8 gMonIcon_LycanrocMidnight[];
 extern const u8 gMonIcon_LycanrocDusk[];
+extern const u8 gMonIcon_Zygarde10[];
+extern const u8 gMonIcon_Zygarde100[];
 
 extern const u8 gMonFootprint_QuestionMark[];
 extern const u8 gMonFootprint_Bulbasaur[];
