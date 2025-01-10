77491c3eb41b6f4ac1c38c1daff5684f4f93ed83
DizzyEggg <jajkodizzy@wp.pl>
2025-01-10 09:36:16 +0100

    mystery gift fix

--- src/mystery_gift_scripts.c
+++ src/mystery_gift_scripts.c
@@ -10,7 +10,7 @@ static const u8 sText_CanceledReadingCard[] = _("Canceled reading\nthe Card.");
 // Client scripts
 //==================
 
-const struct MysteryGiftClientCmd gMysteryGiftClientScript_Init[] = {
+const struct MysteryGiftClientCmd gMysteryGiftClientScript_Init[2] = {
     {CLI_RECV, MG_LINKID_CLIENT_SCRIPT},
     {CLI_COPY_RECV}
 };
